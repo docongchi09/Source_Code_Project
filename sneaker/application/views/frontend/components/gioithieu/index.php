b4f7057d7af366563b9699bbdb22bef6e0e8a65d
Do Cong Chi <17521292@gm.uit.edu.vn>
2023-01-06 14:52:01 +0700

    first commit

---
+++ sneaker/application/views/frontend/components/gioithieu/index.php
@@ -0,0 +1,10 @@
+<center ><h3>• Giới thiệu •<br/>
+<h4>Giày MessiEsport là trang thương mại điện tử chuyên cung cấp và bán lẻ các sản phẩm giày thời trang cao cấp, giày thể thao hàng hiệu hàng đầu tại Việt Nam. <br/>
+
+Đội ngũ Giày MessiEsport!
+	<br/>
+-Được thực hiện bởi sinh viên-
+	<br/>
+Đỗ Công Chí
+	 </h4>
+	<br/></center>
